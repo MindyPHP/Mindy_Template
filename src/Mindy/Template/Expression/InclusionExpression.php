f024abafcbfbe78dbdf0886363852ad27f719f5f
Falaleev Maxim <max@studio107.ru>
2014-10-26 20:01:41 +0300

    Add comments to #2 issue

--- src/Mindy/Template/Expression/InclusionExpression.php
+++ src/Mindy/Template/Expression/InclusionExpression.php
@@ -8,24 +8,25 @@ class InclusionExpression extends LogicalExpression
 {
     public function compile(Compiler $compiler, $indent = 0)
     {
+        // if (is_array($haystack))
         $compiler->raw('(is_array(', $indent);
         $this->right->compile($compiler);
         $compiler->raw(') ? ');
-
+        // {
             $compiler->raw('(in_array(', $indent);
             $this->left->compile($compiler);
             $compiler->raw(', (array)');
             $this->right->compile($compiler);
             $compiler->raw('))');
-
+        // } else
         $compiler->raw(' : ', $indent);
-
+        // {
             $compiler->raw('(mb_strpos(', $indent);
             $this->right->compile($compiler);
             $compiler->raw(', ');
             $this->left->compile($compiler);
             $compiler->raw(') === 0)');
-
+        // }
         $compiler->raw(')');
     }
 }
