678e2ab674460fedb8d1a6d8ae17448f80854a68
Maksim Falaleev <max@studio107.ru>
2016-10-06 00:46:07 +0300

    Remove finder deps

--- src/Renderer.php
+++ src/Renderer.php
@@ -3,7 +3,6 @@
 declare(strict_types = 1);
 
 namespace Mindy\Template;
-use Mindy\Finder\Finder;
 
 /**
  * Class Renderer
@@ -11,13 +10,4 @@ use Mindy\Finder\Finder;
  */
 class Renderer extends Loader
 {
-    /**
-     * Renderer constructor.
-     * @param Finder $finder
-     * @param array $options
-     */
-    public function __construct(Finder $finder, array $options = [])
-    {
-        parent::__construct(array_merge($options, ['source' => $finder->getPaths()]));
-    }
 }
